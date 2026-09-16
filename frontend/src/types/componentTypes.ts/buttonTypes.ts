36b3a13f81a6397aa7509692b83b7a5c89b8606d
KiranNaikS12 <kiransnaik56@gmail.com>
2026-09-16 13:06:13 +0530

    feat: add isDisabeld prop to custom button component

--- frontend/src/types/componentTypes.ts/buttonTypes.ts
+++ frontend/src/types/componentTypes.ts/buttonTypes.ts
@@ -2,6 +2,7 @@ type ButtonType = "submit" | "reset" | "button";
 
 export type ButtonProps = {
     type?: ButtonType ;
+    isDisabled: boolean;
     children: React.ReactNode
     className?: string;
     onClick?: () => void;
